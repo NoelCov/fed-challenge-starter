9a5dbea7a0c1ac858551b9fe363327361b68ce7d
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-03-01 14:33:41 -0800

    Fixed files to be able to host repo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,18 @@ import Timer from "./Timer";
 export default function Card(props) {
     return (
     <div className="card">
-    <img src={props.src} className="card-img-top" />
+    <img src={props.src} className="card-img-top" alt="" />
     {props.style}
     <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        <img className="profImage" src={props.profilePicURL} />
+        <img className="profImage" src={props.profilePicURL} alt="" />
         <Timer
             icon={props.icon}
             time={props.time}
             secondIcon={props.secondIcon} 
             distance={props.distance}
         />
-        <a href="#"><b>{props.link}</b></a>
+        <a href="/"><b>{props.link}</b></a>
     </div>
     </div>
     )
